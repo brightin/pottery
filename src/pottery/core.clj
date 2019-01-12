ee88452bbc65aea62691a7a1f8e0527efe5b4925
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-01-12 17:07:27 +0100

    Update core to conform with new scan api

--- src/pottery/core.clj
+++ src/pottery/core.clj
@@ -2,12 +2,24 @@
   (:require [pottery.po :as po]
             [pottery.scan :as scan]))
 
+(defmacro make-extractor
+  "Returns an extraction function using the core.match pattern syntax.
+
+  Example:
+  ```
+  (make-extractor
+    ['tr s] s
+    ['trn [s1 s2] _] [s1 s2])
+  ```"
+  [& args]
+  `(scan/make-extractor ~@args))
+
 (defn scan-codebase!
   "Recursively reads the code in dir, scans all strings and outputs a
   .pot file according to the gettext format with all the translatable
   strings."
-  [{:keys [dir template-file] :as opts}]
-  (->> (scan/scan-files dir)
+  [{:keys [template-file] :as opts}]
+  (->> (scan/scan-files opts)
        (po/gen-template)
        (spit template-file)))
 
