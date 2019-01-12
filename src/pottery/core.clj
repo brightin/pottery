5b7eb479e9120784a58513316f96cc257c3bfc81
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-01-12 14:31:02 +0100

    Expose a basic API in core ns

--- src/pottery/core.clj
+++ src/pottery/core.clj
@@ -1,6 +1,14 @@
-(ns pottery.core)
+(ns pottery.core
+  (:require [pottery.po :as po]
+            [pottery.scan :as scan]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn scan-codebase!
+  "Recursively reads the code in dir, scans all strings and outputs a
+  .pot file according to the gettext format with all the translatable
+  strings."
+  [{:keys [dir template-file] :as opts}]
+  (->> (scan/scan-files dir)
+       (po/gen-template)
+       (spit template-file)))
+
+(def read-po-file #'po/read-po-file)
