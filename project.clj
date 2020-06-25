f03597267c6cfe48060e42e5bca25b84abb1d0b2
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2020-06-26 00:19:31 +0200

    Update license on Clojars

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject brightin/pottery "0.0.2"
   :description "A clojure library to interact with gettext and PO/POT files"
   :url "https://www.github.com/brightin/pottery"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "Hippocratic License"
+            :url "https://firstdonoharm.dev/"}
   :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
                  [org.clojure/core.match "1.0.0"]]
   :repl-options {:init-ns pottery.core})
